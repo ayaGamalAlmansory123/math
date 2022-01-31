9a4c974ef570eaa476508c54b7e6631e091038d5
ALL IN ONE <ALL IN ONE@DESKTOP-L2GD7PC>
2022-01-31 14:00:49 +0200

    Add project files.

---
+++ testone/sub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testone
+{
+    public  class sub
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public  int subtraction()
+        {
+            return x - y;
+        }
+    }
+}
